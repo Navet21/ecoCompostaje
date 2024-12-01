1ffc226b2b0e6ecb852e036fa18bb22d4755e738
PC-FP <pc@fp.com>
2024-12-01 22:15:23 +0000

    idioma español

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ $user->name ?? __('Show') . ' ' . __('User') }}
+            {{ $user->name ?? __('Mostrar') . ' ' . __('User') }}
         </h2>
     </x-slot>
 
